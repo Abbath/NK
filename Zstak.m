0f8606153d0069f124fdeaa07b037a051c931b49
Abbath <pitongogi@gmail.com>
2015-06-25 23:10:35 +0300

    Recursion to loop

--- Zstak.m
+++ Zstak.m
@@ -1,29 +1,31 @@
 function [ z1, t1 ] = Zstak( Layer, z, t, alfa, f, theta, MaxLay, Nin, want_transmission)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-if Layer <= MaxLay
-    p = Compos(alfa(Layer), z);
-    z = exp(theta(Layer)) * p;
-    if want_transmission
-        if Layer < MaxLay
-            half_thet = theta(Layer) / 2;
-            p_factor = f(Layer) * p;
-            p_factor = complex(1 - real(p_factor), - imag(p_factor));
-            z_factor = f(Layer) * p;
-            z_factor = complex(1 - real(z_factor), - imag(z_factor));
-            temp = t * (p_factor/z_factor);
-            t = exp(half_thet) * temp;
-        else
-            negF = -f(Layer);
-            den = Compos(negF, p);
-            den = f(Layer) * den;
-            den = complex(1 + real(den), imag(den));
-            temp = t / den;
-            n_fac = 2 * Nin / (Nin+1);
-            t = temp * n_fac;
+for i = 1:MaxLay
+    %if Layer <= MaxLay
+        p = Compos(alfa(Layer), z);
+        z = exp(theta(Layer)) * p;
+        if want_transmission
+            if Layer < MaxLay
+                half_thet = theta(Layer) / 2;
+                p_factor = f(Layer) * p;
+                p_factor = complex(1 - real(p_factor), - imag(p_factor));
+                z_factor = f(Layer) * p;
+                z_factor = complex(1 - real(z_factor), - imag(z_factor));
+                temp = t * (p_factor/z_factor);
+                t = exp(half_thet) * temp;
+            else
+                negF = -f(Layer);
+                den = Compos(negF, p);
+                den = f(Layer) * den;
+                den = complex(1 + real(den), imag(den));
+                temp = t / den;
+                n_fac = 2 * Nin / (Nin+1);
+                t = temp * n_fac;
+            end
         end
-    end
-    [z, t] = Zstak(Layer + 1, z, t, alfa, f , theta, MaxLay, Nin, want_transmission);
+        %[z, t] = Zstak(Layer + 1, z, t, alfa, f , theta, MaxLay, Nin, want_transmission);
+    %end
 end
 z1 = z;
 t1 = t;
