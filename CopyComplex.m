b2ddbce31d677aa93d423e18f9e240f2ed0f4112
Abbath <pitongogi@gmail.com>
2015-06-25 17:45:24 +0300

    More fixes

--- CopyComplex.m
+++ CopyComplex.m
@@ -1,6 +1,7 @@
 function [ beta ] = CopyComplex( MaxLay, n )
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
+beta = zeros(1, MaxLay);
 for i = 1:MaxLay
    beta(i) = n(i); 
 end
--- CopyStack.m
+++ CopyStack.m
@@ -1,8 +1,9 @@
 function [ d_to ] = CopyStack( MaxLay, d_from )
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
+d_to = zeros(1, MaxLay);
 for i = 1:MaxLay
-   d_to = d_from; 
+   d_to(i) = d_from(i); 
 end
 
 end
